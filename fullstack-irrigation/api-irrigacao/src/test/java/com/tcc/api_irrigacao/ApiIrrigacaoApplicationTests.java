09abb08ec10ebdcdc5ad1e0e1ad2e71dd1c967cd
PLucenaa <pedro12brasil@gmail.com>
2026-02-04 23:15:30 -0400

    fix: configura testes com H2 e corrige encoding do application.properties

--- fullstack-irrigation/api-irrigacao/src/test/java/com/tcc/api_irrigacao/ApiIrrigacaoApplicationTests.java
+++ fullstack-irrigation/api-irrigacao/src/test/java/com/tcc/api_irrigacao/ApiIrrigacaoApplicationTests.java
@@ -2,12 +2,14 @@ package com.tcc.api_irrigacao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiIrrigacaoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
